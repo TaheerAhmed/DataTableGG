fba53f2938a3d50733f5365c123615e28ca04bc0
TaheerAhmed <85991537+TaheerAhmed@users.noreply.github.com>
2023-04-11 20:11:20 +0530

    style: pretified code base make it more aesthetically pleasing

--- src/App.js
+++ src/App.js
@@ -1,79 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Date from './Components/Date';
-import Table from './Components/Table';
 
+import Analytics from './Components/Analytics';
 function App() {
-  const dummyData = [
-    {
-      date: "2021-07-07T00:00:00Z",
-      app_id: 212211,
-      requests: 36249787,
-      responses: 36329805,
-      impression: 35318801,
-      clicks: 328202,
-      revenue: 5742.423646628184
-    },
-    {
-      date: "2021-07-06T00:00:00Z",
-      app_id: 212112,
-      requests: 46249787,
-      responses: 46329805,
-      impression: 45318801,
-      clicks: 428202,
-      revenue: 6742.423646628184
-    },
-    {
-      date: "2021-07-05T00:00:00Z",
-      app_id: 212113,
-      requests: 56249787,
-      responses: 56329805,
-      impression: 55318801,
-      clicks: 528202,
-      revenue: 7742.423646628184
-    },
-    {
-      date: "2021-07-05T00:00:00Z",
-      app_id: 212113,
-      requests: 56249787,
-      responses: 56329805,
-      impression: 55318801,
-      clicks: 528202,
-      revenue: 7742.423646628184
-    },
-    {
-      date: "2021-07-05T00:00:00Z",
-      app_id: 212113,
-      requests: 56249787,
-      responses: 56329805,
-      impression: 55318801,
-      clicks: 528202,
-      revenue: 7742.423646628184
-    },
-    {
-      date: "2021-05-05T00:00:00Z",
-      app_id: 212213,
-      requests: 56249787,
-      responses: 56329805,
-      impression: 55318801,
-      clicks: 528202,
-      revenue: 7742.423646628184
-    },
-    {
-      date: "2021-06-05T00:00:00Z",
-      app_id: 212213,
-      requests: 56249787,
-      responses: 56329805,
-      impression: 55318801,
-      clicks: 528202,
-      revenue: 7742.423646628184
-    }
-  ];
+
   return (
     <div className="App">
-      hello
-      <Date/>
-      <Table data={dummyData}/>
+      <Analytics/>
     </div>
   );
 }
---
+++ src/Components/Analytics.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import Date from './Date'
+import Table from './Table'
+const Analytics = () => {
+    const dummyData = [
+        {
+            date: "2021-07-07T00:00:00Z",
+            app_id: 212211,
+            requests: 36249787,
+            responses: 36329805,
+            impression: 35318801,
+            clicks: 328202,
+            revenue: 5742.423646628184
+        },
+        {
+            date: "2021-07-06T00:00:00Z",
+            app_id: 212112,
+            requests: 46249787,
+            responses: 46329805,
+            impression: 45318801,
+            clicks: 428202,
+            revenue: 6742.423646628184
+        },
+        {
+            date: "2021-07-05T00:00:00Z",
+            app_id: 212113,
+            requests: 56249787,
+            responses: 56329805,
+            impression: 55318801,
+            clicks: 528202,
+            revenue: 7742.423646628184
+        },
+        {
+            date: "2021-07-05T00:00:00Z",
+            app_id: 212113,
+            requests: 56249787,
+            responses: 56329805,
+            impression: 55318801,
+            clicks: 528202,
+            revenue: 7742.423646628184
+        },
+        {
+            date: "2021-07-05T00:00:00Z",
+            app_id: 212113,
+            requests: 56249787,
+            responses: 56329805,
+            impression: 55318801,
+            clicks: 528202,
+            revenue: 7742.423646628184
+        },
+        {
+            date: "2021-05-05T00:00:00Z",
+            app_id: 212213,
+            requests: 56249787,
+            responses: 56329805,
+            impression: 55318801,
+            clicks: 528202,
+            revenue: 7742.423646628184
+        },
+        {
+            date: "2021-06-05T00:00:00Z",
+            app_id: 212213,
+            requests: 56249787,
+            responses: 56329805,
+            impression: 55318801,
+            clicks: 528202,
+            revenue: 7742.423646628184
+        }
+    ];
+useEffect(() => {
+  
+
+  
+}, [third])
+
+    
+  return (
+      <div>hello
+          <Date />
+          <Table data={dummyData} /></div>
+  )
+}
+
+export default Analytics
