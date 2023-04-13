0e1f70a29e7c3c2d2af35c222d552036e9e1e567
TaheerAhmed <85991537+TaheerAhmed@users.noreply.github.com>
2023-04-14 05:12:45 +0530

    feature:table store initialized github-pg setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import dateSlice from './Slices/dateSlice'
 import settingSlice from './Slices/settingSlice'
+import tableSlice from './Slices/tableSlice'
 const store = configureStore(
   {
-    reducer: { date: dateSlice,setting:settingSlice }
+    reducer: { date: dateSlice,setting:settingSlice,table:tableSlice }
   }
 )
 export default store
