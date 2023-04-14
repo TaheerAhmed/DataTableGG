f0c9a215754e06756e81e26e8b3f192ae5fa2916
TaheerAhmed <85991537+TaheerAhmed@users.noreply.github.com>
2023-04-14 11:37:12 +0530

    feature:filter dates and app_names multiple app names

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,8 +6,10 @@ import { tableActions } from '../store/Slices/tableSlice';
 const Table = ({ data }) => {
   const dispatch = useDispatch()
   const [sortConfig, setSortConfig] = useState(null);
-  // const [filters, setFilters] = useState({});
+  const [selectedOptions, setSelectedOptions] = useState([]);
   const [filterSelect, setFilterSelect] = useState(false);
+  const [appNameSelectedOptions, setAppNameSelectedOptions] = useState([]);
+  const [dateSelectedOptions, setDateSelectedOptions] = useState([]);
   const [sortingState, setSortingState] = useState(null);
   const tempSliderValues= useSelector(state => state.table.tempSliderValues)
   const filters=useSelector(state => state.table.filters)
@@ -41,7 +43,9 @@ const Table = ({ data }) => {
     return data.filter((row) => {
       for (const key in filters) {
         if (key === 'Date' || key === 'App Name') {
-          if (String(row[key]).toLowerCase().indexOf(filters[key].toLowerCase()) === -1) {
+          const filterValues = filters[key].split('|');
+          console.log(filterValues)
+          if (!filterValues.some((value) => String(row[key]).toLowerCase().includes(value.toLowerCase()))) {
             return false;
           }
         } else {
@@ -68,7 +72,12 @@ const Table = ({ data }) => {
       dispatch(tableActions.setTempSliderValues({ ...tempSliderValues, [key]: value }));
       return;
     } else if (selectedOptionOrEvent) {
-      value = selectedOptionOrEvent.value;
+      if(selectedOptionOrEvent.length>0){
+        value = selectedOptionOrEvent.map((option) => option.value).join('|');
+      }
+      else{
+        value = selectedOptionOrEvent.value;
+      }
     } else {
       value = null;
     }
@@ -101,15 +110,45 @@ const Table = ({ data }) => {
       const uniqueValues = getUniqueValues(key);
       const options = uniqueValues.map((value) => ({ label: value, value }));
 
+      const handleApply = () => {
+        if (key === 'App Name') {
+          handleFilterChange(appNameSelectedOptions, key);
+          setAppNameSelectedOptions([]);
+        } else if (key === 'Date') {
+          handleFilterChange(dateSelectedOptions, key);
+          setDateSelectedOptions([]);
+        }
+        setFilterSelect(null);
+      };
+
+      const handleCancel = () => {
+        if (key === 'App Name') {
+          setAppNameSelectedOptions([]);
+        } else if (key === 'Date') {
+          setDateSelectedOptions([]);
+        }
+        setFilterSelect(null);
+      };
+
       return (
-        <Select
-          className="filter-select"
-          placeholder={`Filter ${key}`}
-          options={options}
-          isSearchable
-          onChange={(selectedOption) => handleFilterChange(selectedOption, key)}
-          onBlur={() => setFilterSelect(null)}
-        />
+        <div className='filter-container'>
+          <Select
+            className="filter-select"
+            placeholder={`Filter ${key}`}
+            options={options}
+            isMulti
+            isSearchable
+            value={key === 'App Name' ? appNameSelectedOptions : dateSelectedOptions}
+            onChange={key === 'App Name' ? setAppNameSelectedOptions : setDateSelectedOptions}
+          />
+          <div className='buttons-filter'>
+            <button onClick={handleApply} className='slider-apply-button'>Apply</button>
+            <button onClick={handleCancel} className='slider-reset-button'>Cancel</button>
+          </div>
+          <span className="close-filter-search" onClick={closeFilter}>
+            x
+          </span>
+        </div>
       );
     } else {
     const minValue = Math.min(...data.map((row) => row[key]));
@@ -132,12 +171,13 @@ const Table = ({ data }) => {
           <span className="slider-max-label">Max:{maxValue}</span>
         </div>
         
-        <button className="slider-reset-button" onClick={() => resetSlider(key)}>
+        
+        <div className='buttons-filter'><button className="slider-reset-button" onClick={() => resetSlider(key)}>
           Reset
         </button>
-        <button className="slider-apply-button" onClick={() => applySliderFilters(key)}>
-          Apply
-        </button>
+          <button className="slider-apply-button" onClick={() => applySliderFilters(key)}>
+            Apply
+          </button></div>
         <span className="close-filter" onClick={closeFilter}>x</span>
       </div>
     );
@@ -182,7 +222,6 @@ const Table = ({ data }) => {
   const filteredData = filterData(data, filters, filterSelect);
   const sortedData = sortData(filteredData, sortConfig);
   const formattedHeaderData = totalHeaderData(sortedData);
-console.log("sortedData",sortedData)
   return (
     <div className="table-container">
       <table>
